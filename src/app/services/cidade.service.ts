ea29d0d07068a9fc43e7f7e63c64aed391d6fa8a
lucasjdutra <lucasjdc90@gmail.com>
2024-10-15 23:35:19 -0300

    changes to cidade.service.ts and db.json

--- src/app/services/cidade.service.ts
+++ src/app/services/cidade.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Cidade {
+  id?: number;
+  nome: string;
+  estado: string;
+  populacao: number;
+  capital: boolean;
+  clima: string; // Usado para radio button ou select
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CidadeService {
+  private apiUrl = 'http://localhost:3000/cidades';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCidades(): Observable<Cidade[]> {
+    return this.http.get<Cidade[]>(this.apiUrl);
+  }
+
+  addCidade(cidade: Cidade): Observable<Cidade> {
+    return this.http.post<Cidade>(this.apiUrl, cidade);
+  }
+
+  updateCidade(id: number, cidade: Cidade): Observable<Cidade> {
+    return this.http.put<Cidade>(`${this.apiUrl}/${id}`, cidade);
+  }
+
+  deleteCidade(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
