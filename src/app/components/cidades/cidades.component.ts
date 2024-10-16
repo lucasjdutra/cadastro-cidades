1fc8d507e37a1285268ee4a3d0ac8bbba2b14418
lucasjdutra <lucasjdc90@gmail.com>
2024-10-15 23:40:37 -0300

    changes to 'cidades' component

--- src/app/components/cidades/cidades.component.ts
+++ src/app/components/cidades/cidades.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CidadeService, Cidade } from '../../services/cidade.service';
 
 @Component({
   selector: 'app-cidades',
   templateUrl: './cidades.component.html',
-  styleUrl: './cidades.component.css'
+  styleUrls: ['./cidades.component.css']
 })
-export class CidadesComponent {
+export class CidadesComponent implements OnInit {
+  cidades: Cidade[] = [];
+  cidadeForm: Cidade = {
+    nome: '',
+    estado: '',
+    populacao: 0,
+    capital: false,
+    clima: ''
+  };
+  cidadeSelecionada: Cidade | null = null;
 
+  constructor(private cidadeService: CidadeService) {}
+
+  ngOnInit(): void {
+    this.listarCidades();
+  }
+
+  listarCidades(): void {
+    this.cidadeService.getCidades().subscribe((cidades) => {
+      this.cidades = cidades;
+    });
+  }
+
+  salvarCidade(): void {
+    if (this.cidadeSelecionada) {
+
+      this.cidadeService.updateCidade(this.cidadeSelecionada.id!, this.cidadeForm).subscribe(() => {
+        this.listarCidades();
+        this.resetarFormulario();
+      });
+    } else {
+
+      this.cidadeService.addCidade(this.cidadeForm).subscribe(() => {
+        this.listarCidades();
+        this.resetarFormulario();
+      });
+    }
+  }
+
+  editarCidade(cidade: Cidade): void {
+    this.cidadeSelecionada = cidade;
+    this.cidadeForm = { ...cidade };
+  }
+
+  removerCidade(id: number | undefined): void {
+    if (id) {
+      this.cidadeService.deleteCidade(id).subscribe(() => {
+        this.listarCidades();
+      });
+    }
+  }
+
+  resetarFormulario(): void {
+    this.cidadeSelecionada = null;
+    this.cidadeForm = {
+      nome: '',
+      estado: '',
+      populacao: 0,
+      capital: false,
+      clima: ''
+    };
+  }
 }
